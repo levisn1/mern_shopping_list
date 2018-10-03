f7b21f4f59155d7c588ccbefe6079c60369d1eb5
levisn1 <djlevisn1@gmail.com>
2018-10-03 14:34:02 +0200

    Add model and routes

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser'); //get data from the body, name from a post of instance
 
-const app = express();       //initialize express in a variable
+const items = require('./routes/api/items');
+
+const app = express();    //initialize express in a variable
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
@@ -17,7 +19,11 @@ mongoose
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
+// Use Routes
 
-  const port = process.env.PORT || 5000;
+app.use('/api/items', items);
 
-  app.listen(port, () => console.log(`Server started on port ${port}`));
+
+const port = process.env.PORT || 5000; //we create  a variable with the port we are going to use, process.env.PORT if it's and env variable for HEROKU
+
+app.listen(port, () => console.log(`Server started on port ${port}`)); //we want our app that listen to that port and with a call back send a message
